2b84a3758eadcc07be07cfaabddd28cd0e3912bf
Виталий Любимов <lubimov@itstep.org>
2018-02-05 20:02:54 +0200

    Добавлены класса в DistributionLibrary для работы с сервисом

---
+++ BlockChain-master/DistributionLibrary/PeerEntry.cs
@@ -0,0 +1,17 @@
+﻿using StepCoin.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.PeerToPeer;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistributionLibrary
+{
+    public class PeerEntry
+    {
+        public Node Node { get; set; }
+        public PeerName PeerName { get; set; }
+        public IP2PService ServiceProxy { get; set; }
+    }
+}
